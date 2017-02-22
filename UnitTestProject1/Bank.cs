aa5409b72841dbf51a272aaeef4e9cd517bf387a
chris wright <professionalcandrew@gmail.com>
2017-02-22 13:19:26 -0500

    csharp unit test initial project

---
+++ UnitTestProject1/Bank.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    class Bank
+    {
+        private double balance;
+        public Bank(double balance)
+        {
+            this.balance = balance;
+        }
+        public double gsBalance
+        {
+            get
+            {
+                return this.balance;
+            }
+            set
+            {
+                this.balance = value;
+            }
+        }
+        public void deposit(double money)
+        {
+            this.balance += money;
+        }
+        public void withdraw(double money)
+        {
+            this.balance -= money;
+        }
+        public String bankName()
+        {
+            return "TDBank";
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Bank bOne = new Bank(1500.0);
+            bOne.deposit(300.0);
+            Assert.AreEqual(1800.0, bOne.gsBalance);
+        }
+        [TestMethod]
+        public void testString()
+        {
+            Bank bTwo = new Bank(1500.0);
+            StringAssert.Contains("TDBank", bTwo.bankName());
+        }
+    }
+}
